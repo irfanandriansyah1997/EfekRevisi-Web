5cc3ce31a9a26b5e45ee09b310ce2dd924699d44
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2018-08-09 21:48:44 +0700

    Feature: add codecov & testing eslint

--- web/src/components/Home/WelcomeMessage.vue
+++ web/src/components/Home/WelcomeMessage.vue
@@ -6,11 +6,11 @@
 
 <script>
 export default {
-    props: {
-        name: {
-            type: String,
-            required: true
-        }
+  props: {
+    name: {
+        type: String,
+      required: true
     }
-};
+  }
+}
 </script>
