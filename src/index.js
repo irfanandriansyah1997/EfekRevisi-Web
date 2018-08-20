99bb432ed5bab8a81b9c1d54afd5e0fa512d1b3c
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2018-08-20 21:16:37 +0700

    Feature: implement component in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import router from './router'
 import App from './App'
+import Component from './components'
 
 import 'assets/css/app.styl'
+
+Component.map(x => {
+  Vue.component(x.name, x.component)
+  return x
+})
 
 /* eslint-disable-next-line no-new */
 new Vue({
