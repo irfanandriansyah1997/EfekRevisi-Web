df31a3948d9753eb479236e88d6720a7320a4730
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2018-08-25 12:01:09 +0700

    Feature: create icon component & proptypes file

--- src/components/general/atomic/icon/icon.component.vue
+++ src/components/general/atomic/icon/icon.component.vue
@@ -1,7 +1,42 @@
 <template>
-  <h1>test</h1>
+  <i :class="classes">
+    <slot v-if="type === 'material-icons'" />
+  </i>
 </template>
 
 <style lang="scss">
 @import 'style.scss';
 </style>
+
+<script>
+import props from './prop-types'
+
+export default {
+  components: {
+    Vnodes: {
+      functional: true,
+      render: (h, ctx) => ctx.props.vnodes
+    }
+  },
+  props,
+  computed: {
+    classes() {
+      const className = {
+        'ef-icon': true,
+        'ef-icon--x-small': this.size === 'small',
+        'ef-icon--small': this.size === 'small',
+        'ef-icon--large': this.size === 'large',
+        'ef-icon--x-large': this.size === 'x-large'
+      }
+
+      if (this.type === 'material-icons') {
+        className['material-icons'] = true
+      } else {
+        className[this.$slots.default] = true
+      }
+
+      return className
+    }
+  }
+}
+</script>
---
+++ src/components/general/atomic/icon/prop-types.js
@@ -0,0 +1,24 @@
+export default {
+  color: {
+    type: String,
+    default: null
+  },
+  size: {
+    type: String,
+    default: 'default',
+    validator: value => {
+      var label = ['x-small', 'small', 'default', 'large', 'x-large']
+
+      return label.indexOf(value) > -1
+    }
+  },
+  type: {
+    type: String,
+    default: 'material-icons',
+    validator: value => {
+      var label = ['material-icons', 'fontawesome']
+
+      return label.indexOf(value) > -1
+    }
+  }
+}
