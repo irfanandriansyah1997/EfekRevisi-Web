12f8ee9f5f420d88f3255bc6c84e2832a8d13dbb
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2018-09-09 13:04:01 +0700

    Feature: add resolve path in webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -6,5 +6,20 @@ module.exports = (storybookBaseConfig, configType, defaultConfig) => {
     use: ['vue-style-loader', 'css-loader', 'sass-loader']
   })
 
+  defaultConfig.resolve = {
+    alias: {
+      '@': path.resolve(__dirname, '../src'),
+      assets: path.resolve(__dirname, '../assets'),
+      components: path.resolve(__dirname, '../src/components'),
+      helper: path.resolve(__dirname, '../src/helper'),
+      mixins: path.resolve(__dirname, '../src/mixins'),
+      pages: path.resolve(__dirname, '../src/pages'),
+      router: path.resolve(__dirname, '../src/router'),
+      static: path.resolve(__dirname, '../static'),
+      store: path.resolve(__dirname, '../src/store')
+    },
+    extensions: ['.js', '.jsx', '.css', '.png', '.jpg', '.gif', '.jpeg'],
+  };
+
   return defaultConfig
 }
